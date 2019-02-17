b63c8aaa6f292c88e8b76cade58402f4b75b88b9
thisisfruit <42990686+thisisfruit@users.noreply.github.com>
2019-02-17 16:06:00 +0800

    04-order_drinks_foods _carts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //import module
 var restify = require('restify');
 var builder = require('botbuilder');
+var menuItems = require('./menuConfig.json')
 
 //create web server
 var server = restify.createServer();
@@ -20,25 +21,119 @@ var bot = new builder.UniversalBot(connector)
 
 bot.dialog('/',[
     function(session){
-        session.send('歡迎使用自動飲料訂單機器人'),
-        builder.Prompts.choice(session,"請問今天喝什麼呢？",["紅茶","綠茶","奶茶"],{listStyle:builder.ListStyle.button})},
-    function(session, results){
-        session.dialogData.Favor = results.response.entity;
-        builder.Prompts.choice(session,"請問您要的飲料大小是？",["大杯","中杯","小杯"],{listStyle:builder.ListStyle.button});},
-    function(session, results){
-        session.dialogData.Size = results.response.entity;
-        builder.Prompts.number(session,"請問您要幾杯呢？");},
-    function(session, results){
-        session.dialogData.Number = results.response;
-        builder.Prompts.text(session,"請問您的大名是?")},
-    function(session, results){
-        session.dialogData.Name = results.response;
-        builder.Prompts.text(session,"請問您的連絡電話是?")},
-    function(session, results){
-        session.dialogData.Phone = results.response;
-        builder.Prompts.text(session,"請問您的地址是?")},
-    function(session, results){
-        session.dialogData.Address = results.response;
-        session.endDialog(`您的訂購資料如下：<br>品項 : ${session.dialogData.Favor}<br>尺寸 : ${session.dialogData.Size}<br>數量 : ${session.dialogData.Number}<br>訂購人資訊 : <br>${session.dialogData.Name}<br>${session.dialogData.Phone}<br>${session.dialogData.Address}<br>`);},
+        session.send('歡迎使用自動餐點飲料訂單機器人');
+        session.replaceDialog('mainMenu');},
     ]);
 
+bot.dialog('mainMenu',[
+    function(session, args){
+        var promptText;
+        if(args && args.reprompt){
+            promptText = "請問您要再點些什麼？"
+        }
+        else{
+            var promptText = "請問您要點什麼？"
+            session.conversationData.orders = new Array();
+        }
+        builder.Prompts.choice(session, promptText, menuItems, {listStyle:builder.ListStyle.button});
+    },
+    function(session,results){
+        var key = results.response.entity;
+        var dialogID = menuItems[key];
+        if (key == "結帳"){
+            session.replaceDialog(dialogID);
+        }
+        else{session.beginDialog(dialogID)}
+        ;
+    },
+    
+    function(session, results){
+        var order = results.response;
+        var msg;
+        if (order.item == "drink"){
+            msg = `${order.Favor}${order.Size}X${order.Number}杯`;
+        }
+        else if (order.item == "food"){
+            msg = `${order.foodName}X${order.foodNumber}份`
+        }
+            session.send("您剛剛點了:<br>%s", msg);
+            session.conversationData.orders.push(order);
+            session.replaceDialog('mainMenu',{reprompt:true});
+            }
+    ,
+
+    
+])
+
+bot.dialog('orderDrink',[
+        function(session){
+            session.dialogData.orders = {item: "drink"};
+            builder.Prompts.choice(session,"請問今天喝什麼呢？",["紅茶","綠茶","奶茶"],{listStyle:builder.ListStyle.button})},
+        function(session, results){
+            session.dialogData.orders.Favor = results.response.entity;
+            builder.Prompts.choice(session,"請問您要的飲料大小是？",["大杯","中杯","小杯"],{listStyle:builder.ListStyle.button});},
+        function(session, results){
+            session.dialogData.orders.Size = results.response.entity;
+            builder.Prompts.number(session,"請問您要幾杯呢？");},
+        function(session, results){
+            session.dialogData.orders.Number = results.response;
+            session.endDialogWithResult({
+                response:session.dialogData.orders
+            })}
+        ]);
+
+bot.dialog('orderFood',[
+    
+    function(session){
+        session.dialogData.orders = {item: "food"};
+        builder.Prompts.text(session,"請問要點什麼餐點？");
+        },
+    function(session, results){
+        session.dialogData.orders.foodName = results.response;
+        builder.Prompts.number(session,"請問要點幾份？");
+    },
+    function(session, results){
+        session.dialogData.orders.foodNumber = results.response;
+        session.endDialogWithResult({
+            response: session.dialogData.orders});
+    }
+])
+
+bot.dialog('checkOut', [
+    function(session){
+        session.beginDialog("shipment")
+    },
+    function(session, results){
+        var shipments = session.dialogData.shipments = results.response;
+        var shipmentsText = "訂購人資訊 : <br>" + shipments.Name + "<br>" + shipments.Phone + "<br>" + shipments.Address; 
+        var orders = session.conversationData.orders;
+        var ordersText = "";
+        orders.forEach(order =>{
+            if(order.item == "drink"){
+                ordersText += `<br>${order.Favor}${order.Size}X${order.Number}杯`
+            }
+            else if(order.item == "food"){
+                ordersText += `<br>${order.foodName}X${order.foodNumber}份`
+            }
+        })
+        session.endConversation("%s<br>訂單明細:%s",shipmentsText, ordersText);
+    },
+])
+
+bot.dialog('shipment',[
+        function(session,results){
+            session.dialogData.shipments = {};
+            builder.Prompts.text(session,"請問您的大名是?")},
+        function(session, results){
+            session.dialogData.shipments.Name = results.response;
+            builder.Prompts.text(session,"請問您的連絡電話是?")},
+        function(session, results){
+            session.dialogData.shipments.Phone = results.response;
+            builder.Prompts.text(session,"請問您的地址是?")},
+        function(session, results){
+            session.dialogData.shipments.Address = results.response;
+            session.endDialogWithResult({
+                response:session.dialogData.shipments
+            })}
+        ]);
+        
